98c13edd908297e3db0d035a6c8ee5dee4a0aaea
Chris Hood <christopher.hood@gmail.com>
2021-05-05 21:05:33 +1000

    Completed Day 009

--- code/day009/day009project.py
+++ code/day009/day009project.py
@@ -8,7 +8,7 @@ bidders = {}
 
 while more_bidders:
     name = input("What is your name? ")
-    bid = input("What's your bid? $")
+    bid = int(input("What's your bid? $"))
     bidders[name] = bid
     more = input("Are there other bidders? ")
     if more == "no":
@@ -16,5 +16,18 @@ while more_bidders:
     else:
         os.system('cls')
 
-max_bid = max(bidders, key=bidders.get)
-print(f"The winner is {max_bid} with a bid of ${bidders[max_bid]}")
+# this is the cheat way
+# max_bid = max(bidders, key=bidders.get)
+# print(f"The winner is {max_bid} with a bid of ${bidders[max_bid]}")
+
+# this is the way that I thought would not be suitable, but was
+def find_highest_bidder(bidders):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidders:
+        bid = bidders[bidder]
+        if bid > highest_bid:
+            highest_bid = bid
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+find_highest_bidder(bidders)
